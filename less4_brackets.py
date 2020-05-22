cb32f2e68d9dd9f92ae5f77f66b5b8835e5ee4d8
Krugger1982 <62908862+Krugger1982@users.noreply.github.com>
2020-05-22 12:42:19 +0300

    Update less4_brackets.py

--- less4_brackets.py
+++ less4_brackets.py
@@ -1,11 +1,11 @@
 def Brackets(string):
     br = Stack()
     for i in range(len(string)):
-        try:
-            if string[i] == '(':
-                br.push(1)
-            elif string[i] == ')':
-                br.pop()
-        except IndexError:
+        target = 1
+        if string[i] == '(':
+            br.push(1)
+        elif string[i] == ')':
+            target = br.pop()
+        if target == None:
             return False
     return br.size() == 0
