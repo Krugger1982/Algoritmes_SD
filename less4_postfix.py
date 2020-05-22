78a946868cb31a7fbc9f2a35306665e3fe04bedd
Krugger1982 <62908862+Krugger1982@users.noreply.github.com>
2020-05-22 11:16:06 +0300

    Update less4_postfix.py

--- less4_postfix.py
+++ less4_postfix.py
@@ -3,16 +3,16 @@ def postfix_math(expression):
     while expression.size() > 0:
         current = expression.pop()
         if current.isdigit():
-            S1.push(current)
+            S1.push(float(current))
         elif current == '+':
             rezult = 0
             while S1.size() > 0:
-                rezult += float(S1.pop())
+                rezult += S1.pop()
             S1.push(rezult)
         elif current == '*':
             rezult = 1
             while S1.size() > 0:
-                rezult *= float(S1.pop())
+                rezult *= S1.pop()
             S1.push(rezult)
         elif current == '=':
             return S1.pop()
