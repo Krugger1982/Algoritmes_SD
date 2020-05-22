8fea81e63085e303e5842e028afa25067f88452f
Krugger1982 <62908862+Krugger1982@users.noreply.github.com>
2020-05-22 10:38:27 +0300

    Create less4_postfix.py

---
+++ less4_postfix.py
@@ -0,0 +1,19 @@
+def postfix_math(expression):
+    S1 = Stack()
+    while expression.size() > 0:
+        current = expression.pop()
+        if current.isdigit():
+            S1.push(current)
+        elif current == '+':
+            rezult = 0
+            while S1.size() > 0:
+                rezult += float(S1.pop())
+            S1.push(rezult)
+        elif current == '*':
+            rezult = 1
+            while S1.size() > 0:
+                rezult *= float(S1.pop())
+            S1.push(rezult)
+        elif current == '=':
+            return S1.pop()
+    return S1.pop()
