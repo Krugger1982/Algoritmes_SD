f568b707247e94ea7178345e2406fa4e63063700
Krugger1982 <62908862+Krugger1982@users.noreply.github.com>
2020-05-22 13:06:31 +0300

    Update less4_postfix_tests.py

--- less4_postfix_tests.py
+++ less4_postfix_tests.py
@@ -3,20 +3,12 @@ import unittest
 class MyTestCase(unittest.TestCase):    
     
     def test_1(self):
-        C = '82+5*9+='
-        C1 = C[::-1]
-        S = Stack()
-        for i in C1:
-            S.push(i)
-        self.assertEqual(postfix_math(S), 59.0)
+        C1 = '82+5*5+5/10-='
+        self.assertEqual(postfix_math(C1), 1.0)
         
     def test_without_equality_sign(self):
-        C = '82+5*9+'
-        C1 = C[::-1]
-        S = Stack()
-        for i in C1:
-            S.push(i)
-        self.assertEqual(postfix_math(S), 59.0)            
+        C1 = '82+5*5+5/10-'
+        self.assertEqual(postfix_math(C1), 1)            
 
 if __name__ == '__main__':
     try: unittest.main()
