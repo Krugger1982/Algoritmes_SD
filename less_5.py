ae7e6a028be9a9f914b16e617d0a7436efa36b5b
Krugger1982 <62908862+Krugger1982@users.noreply.github.com>
2020-05-27 19:27:55 +0300

    Update less_5.py

--- less_5.py
+++ less_5.py
@@ -1,35 +1,18 @@
-class Stack:
-    def __init__(self):
-        self.stack = []
-
-    def size(self):
-        return len(self.stack)
-
-    def pop(self):
-        x = None
-        try:
-            x = self.stack[0]
-            del self.stack[0]
-        except IndexError:
-            pass
-        return x     
-    
-    def push(self, value):
-        self.stack.insert (0, value)
-
 class Queue:
     def __init__(self):
-        self.s1 = Stack()
-        self.s2 = Stack()
+        self.queue = []
 
     def enqueue(self, item):
-        self.s1.push(item)
-
+        self.queue.append(item)
+        
     def dequeue(self):
-        if  self.s2.size() == 0:
-            while self.s1.size() > 0:
-                self.s2.push(self.s1.pop())
-        return self.s2.pop() 
+        x = None
+        try:
+            x = self.queue[0]
+            del self.queue[0]
+        except IndexError:
+            pass
+        return x   
 
     def size(self):
-        return self.s1.size() + self.s2.size()
+        return len(self.queue)
