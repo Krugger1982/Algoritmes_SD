df4502e6968c8f5eda19ac479a762f0bb423bc4d
Krugger1982 <62908862+Krugger1982@users.noreply.github.com>
2020-05-30 00:18:53 +0300

    Create less_6_tests.py

---
+++ less_6_tests.py
@@ -0,0 +1,57 @@
+import random
+import unittest
+
+class MyTestCase(unittest.TestCase):    
+    def test_size(self):
+        Ex = Deque()
+        Example = [5, 17, 69, 36, 2, 0, 16, 10, 99, 38, 15, 'fourteen']
+        for i in Example:
+            Ex.addFront(i)
+        self.assertEqual(Ex.size(), 12)
+    
+    def test_addFront_removeFront_1(self):
+        Ex = Deque()
+        Example = [5, 17, 69, 36, 2, 0, 16, 10, 99, 38, 15, 'fourteen']
+        for i in Example:
+            Ex.addFront(i)
+        self.assertEqual(Ex.size(), len(Example))
+        for j in reversed(Example):
+            self.assertEqual(Ex.removeFront(), j)
+
+    def test_addFront_removeFront(self):
+        Ex = Deque()
+        for i in range(1000):
+            Example = []
+            for j in range(random.randint(1, 50)):
+                X = random.randint(1, 99)
+                Example.append(X)
+                Ex.addFront(X)
+            self.assertEqual(Ex.size(), len(Example))
+            for j in reversed(Example):
+                self.assertEqual(Ex.removeFront(), j)
+
+                
+    def test_addTail_removeTail_1 (self):
+        Ex = Deque()
+        Example = [5, 17, 69, 36, 2, 0, 16, 10, 99, 38, 15, 'fourteen']
+        for i in Example:
+            Ex.addTail(i)
+        self.assertEqual(Ex.size(), len(Example))
+        for j in Example:
+            self.assertEqual(Ex.removeFront(), j)
+
+    def test_addTail_removeTail(self):
+        Ex = Deque()
+        for i in range(1000):
+            Example = []
+            for j in range(random.randint(1, 50)):
+                X = random.randint(1, 99)
+                Example.append(X)
+                Ex.addTail(X)
+            self.assertEqual(Ex.size(), len(Example))
+            for j in Example:
+                self.assertEqual(Ex.removeFront(), j)        
+    
+if __name__ == '__main__':
+    try: unittest.main()
+    except SystemExit: pass
