872ce1ad2ff87be7a563f6bb9b21b4b92e3cf742
Krugger1982 <62908862+Krugger1982@users.noreply.github.com>
2020-06-27 00:53:52 +1000

    Create less_9.py

---
+++ less_9.py
@@ -0,0 +1,31 @@
+class NativeDictionary:
+    def __init__(self, sz):
+        self.size = sz
+        self.slots = [None] * self.size
+        self.values = [None] * self.size       
+
+    def hash_fun(self, key):
+        index = 0
+        for i in key:
+            index += ord(i)
+        return index % self.size
+
+    def is_key(self, key):
+        return self.slots.count(key) != 0
+            
+    def put(self, key, value):
+        index = self.hash_fun(key)
+        N = 0
+        while N == 0:
+            if self.slots[index] is None:
+                self.slots[index] = key
+                self.values[index] = value
+                N += 1
+            else:
+                index = (index + 1) % self.size
+
+    def get(self, key):
+        if self.is_key(key):
+            return self.values[self.slots.index(key)]
+        else:
+            return None
