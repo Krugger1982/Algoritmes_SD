e70b4501957d2406b35ba7c62bf95d597412ffc8
Krugger1982 <62908862+Krugger1982@users.noreply.github.com>
2020-08-02 15:48:14 +0300

    Update Less_11_Tests.py

--- Less_11_Tests.py
+++ Less_11_Tests.py
@@ -10,14 +10,14 @@ class MyTestCase(unittest.TestCase):
         for i in range(N):
             Set1.put(i)
         self.assertEqual(N, Set1.size())    # проверка что мощность множества равна N
-        for i in range(N//100):               # Снова добавляем чисел (чтоб повторялись)
+        for i in range(N//10):               # Снова добавляем чисел (чтоб повторялись)
             Set1.put(i)
         self.assertEqual(N, Set1.size())    # проверка что мощность множества снова равна N и не изменилась
 
     def test_2_remove(self):
         # создаем тестовое множество из последовательных (неповторяющихся) чисел
         Set1 = PowerSet()
-        N = 150000
+        N = 35000
         M = N//2
         for i in range(N):
             Set1.put(i)
@@ -33,18 +33,17 @@ class MyTestCase(unittest.TestCase):
         Set1 = PowerSet()
         # создаем второe множество 
         Set2 = PowerSet()
-        N = 10
-        M = 7
+        N = 100
+        M = 9
         for i in range(N):
             Set1.put(i)
         for i in range(M, M+N):
             Set2.put(i)                     # Второе множество наполняем числами от  M до N + M, то есть от 70 до 169
-        Set1.intersection(Set2)
-        self.assertEqual(Set1.size(), N - M)    # Проверка по размеру множества (равно N-M) 
+        Set3 = Set1.intersection(Set2)
+        self.assertEqual(Set3.size(), N - M)    # Проверка по размеру множества (равно N-M) 
         for i in range (M, N):
-            self.assertTrue(Set1.get(i))        # Проверка по содержимому (содержит числа от М до N)
-
-
+            self.assertTrue(Set3.get(i))        # Проверка по содержимому (содержит числа от М до N)
+        
     def test_4_union(self):
         Set1 = PowerSet()
         Set2 = PowerSet()
@@ -54,10 +53,10 @@ class MyTestCase(unittest.TestCase):
             Set1.put(i)
         for i in range(M, M+N, 1):
             Set2.put(i)
-        Set1.union(Set2)
-        self.assertEqual(Set1.size(), N + M)    # Проверка по размеру множества
+        Set3 = Set1.union(Set2)
+        self.assertEqual(Set3.size(), N + M)    # Проверка по размеру множества
         for i in range (M+N):
-            self.assertTrue(Set1.get(i))        # Проверка по содержимому (содержит числа от 0 до N + M)
+            self.assertTrue(Set3.get(i))        # Проверка по содержимому (содержит числа от 0 до N + M)
 
     def test_4_1_union_with_empty(self):
         Set1 = PowerSet()
@@ -65,8 +64,8 @@ class MyTestCase(unittest.TestCase):
         N = 100
         for i in range(N):
             Set1.put(i)
-        Set1.union(Set2)
-        self.assertEqual(Set1.size(), N)        # Проверка по размеру множества (не изменилось)
+        Set3 = Set1.union(Set2)
+        self.assertEqual(Set3.size(), N)        # Проверка по размеру множества (не изменилось)
 
     def test_5_difference(self):
         # создаем тестовое множество из последовательных (неповторяющихся) чисел от 0 до N-1
@@ -79,11 +78,26 @@ class MyTestCase(unittest.TestCase):
             Set1.put(i)
         for i in range(M, M+N, 1):
             Set2.put(i)                     # Второе множество наполняем числами от  M до N + M, то есть от 70 до 169
-        Set1.difference(Set2)
-        self.assertEqual(Set1.size(), M)    # Проверка по размеру множества (равно 70) 
+        Set3 = Set1.difference(Set2)
+        self.assertEqual(Set3.size(), M)    # Проверка по размеру множества (равно 70) 
         for i in range (M):
-            self.assertTrue(Set1.get(i))        # Проверка по содержимому (содержит числа от 0 до M)
+            self.assertTrue(Set3.get(i))        # Проверка по содержимому (содержит числа от 0 до M)
 
+    def test_5_1_difference_Equal_Sets(self):
+        # создаем тестовое множество из последовательных (неповторяющихся) чисел от 0 до N-1
+        Set1 = PowerSet()
+        # создаем второe множество 
+        Set2 = PowerSet()
+        N = 100
+        M = 100
+        for i in range(N):
+            Set1.put(i)
+        for i in range(M):
+            Set2.put(i)                     # Второе множество наполняем числами от  M до N + M, то есть от 70 до 169
+        Set3 = Set1.difference(Set2)
+        self.assertEqual(Set3.size(), 0)    # Проверка по размеру множества (пустое) 
+
+            
     def test_6_1_issubset(self):
         # создаем тестовое множество из последовательных (неповторяющихся) чисел от 0 до N-1
         Set1 = PowerSet()
