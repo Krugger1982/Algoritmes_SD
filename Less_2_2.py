9c2033d66a691941d65348b0f96b293154d7cc85
Krugger1982 <62908862+Krugger1982@users.noreply.github.com>
2020-08-21 21:37:39 +0300

    Update Less_2_2.py

--- Less_2_2.py
+++ Less_2_2.py
@@ -64,7 +64,7 @@ class BST:
     def FinMinMax(self, FromNode, FindMax):
         # ищем максимальный/минимальный ключ в поддереве
         if FromNode is None:
-            return None
+            return None                                         # для пустого дерева
         if FindMax and FromNode.RightChild is None:
             return FromNode.NodeKey
         elif FindMax:   
@@ -81,13 +81,13 @@ class BST:
             return False
         # Рассмотрим удаление корня
         if removed.Node.Parent is None and removed.Node.LeftChild is None and removed.Node.RightChild is None:      # если корень - лист
-            self.Root = None    # в итоге - пустое дерево
+            self.Root = None                                                            # в итоге - пустое дерево           
         elif removed.Node.Parent is None and removed.Node.LeftChild is None:            # если у корня есть только правый потомок
             self.Root = removed.Node.RightChild
             removed.Node.RightChild.Parent = None                                       # Этот потомок становится корнем
         elif removed.Node.Parent is None and removed.Node.RightChild is None:           # если у корня есть только левый потомок
             self.Root = removed.Node.LeftChild
-            removed.Node.LeftChild.Parent = None                                       # Этот потомок становится корнем
+            removed.Node.LeftChild.Parent = None                                        # Этот потомок становится корнем
         elif removed.Node.Parent is None:                                               # Если у корня 2 потомка
             heir = self.FindNodeByKey(self.FinMinMax(removed.Node.RightChild, False))   # Находим преемника
             if heir.Node.RightChild is not None:                            # Если у преемника есть правый сын
@@ -134,6 +134,8 @@ class BST:
             if heir.Node.RightChild is not None:                                            # механизм замещения, если у преемника есть правый потомок
                 heir.Node.RightChild.parent = heir.Node.Parent            
                 heir.Node.Parent.LeftChild = heir.Node.RightChild
+            else:
+                heir.Node.Parent.LeftChild = None                                           # А если преемник - лист, то у его родителя обнуляем ссылку на heir.Node
             heir.Node.Parent = removed.Node.Parent                                          # переносим связи с родителем
             heir.Node.Parent.RightChild = heir.Node
             heir.Node.LeftChild = removed.Node.LeftChild                                    # и связи с детьми
@@ -141,7 +143,7 @@ class BST:
             heir.Node.RightChild = removed.Node.RightChild
             heir.Node.RightChild.Parent = heir.Node
         return True
-
+            
     def Count_branch(self, Node):
         counter = 0
         if Node.LeftChild is not None:                      # Если проверяемый узел имеет левого потомка
