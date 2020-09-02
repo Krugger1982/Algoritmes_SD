fd21d2330023a3bbf287177c45606a344c81a1e9
Krugger1982 <62908862+Krugger1982@users.noreply.github.com>
2020-09-02 03:40:52 +0300

    Update Less_2_4.py

--- Less_2_4.py
+++ Less_2_4.py
@@ -4,46 +4,41 @@ class aBST:
         # правильно рассчитайте размер массива для дерева глубины depth:
         tree_size = 2 ** (depth + 1)- 1
         self.Tree = [None] * tree_size # массив ключей
-	
+
     def FindKeyIndex(self, key):
         # ищем в массиве индекс ключа
         index = 0
-        if self.Tree[index] is None:
-            return -len(self.Tree)
-        if key == self.Tree[index]:
+        if self.Tree[index] is None or self.Tree[index] == key:
             return index
-        elif key < self.Tree[index] and len(self.Tree) > 1:
-            # дополнительно проверяем что находимся не на последнем уровне(то есть есть еще "потомки")
-            return self.FindKeyBranch(2 * index + 1, key)
-        elif key > self.Tree[index] and len(self.Tree) > 1: 
-            return self.FindKeyBranch(2 * index + 2, key)
-        return None             # ключ не найден
+        if key < self.Tree[index] and len(self.Tree) > 1:       # если ключ меньше проверяемого и дерево не единичный лист (есть потомки)
+            return self.FindKeyIndexBranch(1, key)              #      то ищем в левой ветке
+        elif key > self.Tree[index] and len(self.Tree) > 1:     # если ключ больше проверяемого и дерево не единичный лист (есть потомки)
+            return self.FindKeyIndexBranch(2, key)              #      то ищем в правой ветке
+        return None                                             # если ничего не найдено
+	
+    def FindKeyIndexBranch(self, branch, key):
+        # ищем в ветке индекс ключа
+        if self.Tree[branch] == key:
+            return branch
+        elif self.Tree[branch] is None:                                         # если вместо key найдено подходящее место для ключа
+            return - branch                                                     # тогда возвращаем отрицательный индекс
+        elif self.Tree[branch] > key and branch < (len(self.Tree) - 1) / 2:     # Если уровень не последний и key меньше проверяемого ключа
+            return self.FindKeyIndexBranch(2 * branch + 1, key)                 #      то ищем в левой ветке
+        elif self.Tree[branch] < key and branch < (len(self.Tree) - 1) / 2:     # Если уровень не последний и key больше проверяемого ключа
+            return self.FindKeyIndexBranch(2 * branch + 2, key)                 #      то ищем в правой ветке
+        return None                                                             # если ничего не найдено
     
-    def FindKeyBranch(self, index, key):
-        if self.Tree[index] is None:
-        # найден свободный слот
-            return -(len(self.Tree) - index)
-        if key == self.Tree[index]:
-        # найден ключ
-            return index
-        elif key < self.Tree[index] and index < ((len(self.Tree) + 1) / 2)-1:
-            # дополнительно проверяем что находимся не на последнем уровне(то есть существуют еще "потомки")
-            return self.FindKeyBranch(2 * index + 1, key)
-            # искомый ключ меньше текущего - ищем в "левой" ветке
-        elif key > self.Tree[index] and index < ((len(self.Tree) + 1) / 2)-1: 
-            return self.FindKeyBranch(2 * index + 2, key)
-            # искомый ключ больше текущего - ищем в "правой" ветке
-        else:
-            return None     # если дошли до последнего уровня (листьев) и ключа там нет, и места для вставки тоже нет
-
-                     
     def AddKey(self, key):
         # добавляем ключ в массив
         index = self.FindKeyIndex(key)
-        if index is None:
-            return -1; 
-            # индекс добавленного/существующего ключа или -1 если не удалось
-        elif index < 0:             # если index отрицтельный, значит есть место для вставки ключа
-            self.Tree[index] = key  # вставляем ключ
-            index = len(self.Tree) + index  # и приводим index к нормальному виду            
-        return index
+        if index == 0 and self.Tree[index] is None:     # если результат поиска - 0 и дерево пустое
+            self.Tree[index] = key
+            return index
+        elif index is None:         # если нет места для вставки и нет совпадений
+            return -1
+        elif index < 0:             # если есть место для вставки
+            index = -1 * index
+            self.Tree[index] = key
+            return index
+        else:                       # если данный ключ уже есть в дереве (в том числе и на позиции [0])
+            return index
